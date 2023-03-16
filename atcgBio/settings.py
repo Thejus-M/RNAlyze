c0c048f9d76b4ad4bb4df6f3574a78e92f308ac5
Thejus-M <git@tigerraw22.anonaddy.com>
2023-03-16 21:12:28 +0530

    bug: whitenoise added

--- atcgBio/settings.py
+++ atcgBio/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # apps
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
+    'whitenoise.runserver_nostatic',  
     
 ]
 
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'atcgBio.urls'
@@ -125,6 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
